0e62d22333f338eab71e97035086d00fab413806
Rokas <rookas.rudzenskas@gmail.com>
2023-08-05 17:26:16 +0300

    FIX

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
-// import LeftSidebar from "@/components/shared/LeftSidebar";
-// import Bottombar from "@/components/shared/Bottombar";
-// import RightSidebar from "@/components/shared/RightSidebar";
-// import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,10 +30,10 @@ export default function RootLayout({
     >
       <html lang='en'>
         <body className={inter.className}>
-          {/*<Topbar />*/}
+          <Topbar />
 
           <main className='flex flex-row'>
-            {/*<LeftSidebar />*/}
+            <LeftSidebar />
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,8 +15,8 @@ async function Home({
   const user = await currentUser();
   if (!user) return null;
 
-  const userInfo = await fetchUser(user.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
+  // const userInfo = await fetchUser(user.id);
+  // if (!userInfo?.onboarded) redirect("/onboarding");
   //
   // const result = await fetchPosts(
   //   searchParams.page ? +searchParams.page : 1,
