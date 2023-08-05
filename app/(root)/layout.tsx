25b64cf17eecbc958d182564bc19939f99d8c27b
Rokas <rookas.rudzenskas@gmail.com>
2023-08-05 17:55:49 +0300

    FIX

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -38,10 +38,10 @@ export default function RootLayout({
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
             {/* @ts-ignore */}
-            {/*<RightSidebar />*/}
+            <RightSidebar />
           </main>
 
-          {/*<Bottombar />*/}
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -9,12 +9,12 @@ async function RightSidebar() {
   const user = await currentUser();
   if (!user) return null;
 
-  const similarMinds = await fetchUsers({
-    userId: user.id,
-    pageSize: 4,
-  });
-
-  const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
+  // const similarMinds = await fetchUsers({
+  //   userId: user.id,
+  //   pageSize: 4,
+  // });
+  //
+  // const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
 
   return (
     <section className='custom-scrollbar rightsidebar'>
@@ -24,46 +24,46 @@ async function RightSidebar() {
         </h3>
 
         <div className='mt-7 flex w-[350px] flex-col gap-9'>
-          {suggestedCOmmunities.communities.length > 0 ? (
-            <>
-              {suggestedCOmmunities.communities.map((community) => (
-                <UserCard
-                  key={community.id}
-                  id={community.id}
-                  name={community.name}
-                  username={community.username}
-                  imgUrl={community.image}
-                  personType='Community'
-                />
-              ))}
-            </>
-          ) : (
-            <p className='!text-base-regular text-light-3'>
-              No communities yet
-            </p>
-          )}
+          {/*{suggestedCOmmunities.communities.length > 0 ? (*/}
+          {/*  <>*/}
+          {/*    {suggestedCOmmunities.communities.map((community) => (*/}
+          {/*      <UserCard*/}
+          {/*        key={community.id}*/}
+          {/*        id={community.id}*/}
+          {/*        name={community.name}*/}
+          {/*        username={community.username}*/}
+          {/*        imgUrl={community.image}*/}
+          {/*        personType='Community'*/}
+          {/*      />*/}
+          {/*    ))}*/}
+          {/*  </>*/}
+          {/*) : (*/}
+          {/*  <p className='!text-base-regular text-light-3'>*/}
+          {/*    No communities yet*/}
+          {/*  </p>*/}
+          {/*)}*/}
         </div>
       </div>
 
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
         <div className='mt-7 flex w-[350px] flex-col gap-10'>
-          {similarMinds.users.length > 0 ? (
-            <>
-              {similarMinds.users.map((person) => (
-                <UserCard
-                  key={person.id}
-                  id={person.id}
-                  name={person.name}
-                  username={person.username}
-                  imgUrl={person.image}
-                  personType='User'
-                />
-              ))}
-            </>
-          ) : (
-            <p className='!text-base-regular text-light-3'>No users yet</p>
-          )}
+          {/*{similarMinds.users.length > 0 ? (*/}
+          {/*  <>*/}
+          {/*    {similarMinds.users.map((person) => (*/}
+          {/*      <UserCard*/}
+          {/*        key={person.id}*/}
+          {/*        id={person.id}*/}
+          {/*        name={person.name}*/}
+          {/*        username={person.username}*/}
+          {/*        imgUrl={person.image}*/}
+          {/*        personType='User'*/}
+          {/*      />*/}
+          {/*    ))}*/}
+          {/*  </>*/}
+          {/*) : (*/}
+          {/*  <p className='!text-base-regular text-light-3'>No users yet</p>*/}
+          {/*)}*/}
         </div>
       </div>
     </section>
