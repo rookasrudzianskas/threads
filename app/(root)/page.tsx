ae792bfc95e2f3a6973060de26efc56ff91cd4a7
Rokas <rookas.rudzenskas@gmail.com>
2023-08-05 17:20:41 +0300

    FIX

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,22 @@
-// import { currentUser } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-//
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 // import ThreadCard from "@/components/cards/ThreadCard";
 // import Pagination from "@/components/shared/Pagination";
-//
+
 // import { fetchPosts } from "@/lib/actions/thread.actions";
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Home({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
-  // const user = await currentUser();
-  // if (!user) return null;
-  //
-  // const userInfo = await fetchUser(user.id);
-  // if (!userInfo?.onboarded) redirect("/onboarding");
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
   //
   // const result = await fetchPosts(
   //   searchParams.page ? +searchParams.page : 1,
