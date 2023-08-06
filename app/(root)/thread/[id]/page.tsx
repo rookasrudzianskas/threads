3fa623079abf5d4250e1c143dc2dc8a6ac22f2ff
Rokas <rookas.rudzenskas@gmail.com>
2023-08-06 12:20:03 +0300

    FIX

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -23,41 +23,7 @@ async function page({ params }: { params: { id: string } }) {
   return (
     <section className='relative'>
       <div>
-        <ThreadCard
-          id={thread._id}
-          currentUserId={user.id}
-          parentId={thread.parentId}
-          content={thread.text}
-          author={thread.author}
-          community={thread.community}
-          createdAt={thread.createdAt}
-          comments={thread.children}
-        />
-      </div>
 
-      <div className='mt-7'>
-        <Comment
-          threadId={params.id}
-          currentUserImg={user.imageUrl}
-          currentUserId={JSON.stringify(userInfo._id)}
-        />
-      </div>
-
-      <div className='mt-10'>
-        {thread.children.map((childItem: any) => (
-          <ThreadCard
-            key={childItem._id}
-            id={childItem._id}
-            currentUserId={user.id}
-            parentId={childItem.parentId}
-            content={childItem.text}
-            author={childItem.author}
-            community={childItem.community}
-            createdAt={childItem.createdAt}
-            comments={childItem.children}
-            isComment
-          />
-        ))}
       </div>
     </section>
   );
