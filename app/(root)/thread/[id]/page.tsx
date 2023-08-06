ee08a4076aec8e2b9a85721dd4f717cc6aeb29e4
Rokas <rookas.rudzenskas@gmail.com>
2023-08-06 12:52:08 +0300

    FIX

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -34,6 +34,31 @@ async function page({ params }: { params: { id: string } }) {
           comments={thread.children}
         />
       </div>
+
+      <div className="mt-7">
+        <Comment
+          threadId={params.id}
+          currentUserImg={user.imageUrl}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
+      </div>
     </section>
   );
 }
