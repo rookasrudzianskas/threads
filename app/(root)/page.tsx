ba7432d85580d4140d6578e0693bdf4e01454960
Rokas <rookas.rudzenskas@gmail.com>
2023-08-06 11:36:51 +0300

    FIX

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import ThreadCard from "@/components/cards/ThreadCard";
-// import Pagination from "@/components/shared/Pagination";
+import ThreadCard from "@/components/cards/ThreadCard";
+import Pagination from "@/components/shared/Pagination";
 
-// import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchPosts } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Home({
@@ -15,39 +15,39 @@ async function Home({
   const user = await currentUser();
   if (!user) return null;
 
-  // const userInfo = await fetchUser(user.id);
-  // if (!userInfo?.onboarded) redirect("/onboarding");
-  //
-  // const result = await fetchPosts(
-  //   searchParams.page ? +searchParams.page : 1,
-  //   30
-  // );
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
 
-      {/*<section className='mt-9 flex flex-col gap-10'>*/}
-      {/*  {result.posts.length === 0 ? (*/}
-      {/*    <p className='no-result'>No threads found</p>*/}
-      {/*  ) : (*/}
-      {/*    <>*/}
-      {/*      {result.posts.map((post) => (*/}
-      {/*        <ThreadCard*/}
-      {/*          key={post._id}*/}
-      {/*          id={post._id}*/}
-      {/*          currentUserId={user.id}*/}
-      {/*          parentId={post.parentId}*/}
-      {/*          content={post.text}*/}
-      {/*          author={post.author}*/}
-      {/*          community={post.community}*/}
-      {/*          createdAt={post.createdAt}*/}
-      {/*          comments={post.children}*/}
-      {/*        />*/}
-      {/*      ))}*/}
-      {/*    </>*/}
-      {/*  )}*/}
-      {/*</section>*/}
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
 
       {/*<Pagination*/}
       {/*  path='/'*/}
