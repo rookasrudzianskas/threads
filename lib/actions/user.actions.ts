b7a0006795e1715a0e51594f1e5b8ad5f319aba7
Rokas <rookas.rudzenskas@gmail.com>
2023-08-06 11:04:32 +0300

    FIX:

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,10 +13,7 @@ export async function fetchUser(userId: string) {
   try {
     connectToDB();
 
-    return await User.findOne({ id: userId }).populate({
-      path: "communities",
-      model: Community,
-    });
+
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
   }
