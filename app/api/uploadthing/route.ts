3ab4ead7725f3004934c9fd2b10812f6f646cc0b
Rokas <rookas.rudzenskas@gmail.com>
2023-08-06 09:57:25 +0300

    FIX

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,5 +1,5 @@
 // Resource: https://docs.uploadthing.com/nextjs/appdir#create-a-nextjs-api-route-using-the-filerouter
-// Copy paste (be careful with imports)
+// Copy and paste (be careful with imports)
 
 import { createNextRouteHandler } from "uploadthing/next";
 
